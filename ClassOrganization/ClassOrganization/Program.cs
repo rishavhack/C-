43aa0bd894e022d5094364f9287a5386c35885a7
Rishav <rishav.jnit@gmail.com>
2018-09-09 20:55:15 +0530

    Class Organization

---
+++ ClassOrganization/ClassOrganization/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassOrganization.com.studentManagment;
+
+namespace ClassOrganization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student std = new student();
+            std.name = "Rishav";
+            std.age = 88;
+            std.detail();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassOrganization/ClassOrganization/com/studentManagment/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOrganization.com.studentManagment
+{
+    class student
+    {
+        public string name;
+        public int age;
+        public void detail()
+        {
+            Console.WriteLine("My Name is {0} and age is {1}",name,age);
+        }
+    }
+}
