5f6b84a71026f3ec75a25d14fd5bebb3e95a1cf7
Rishav <rishav.jnit@gmail.com>
2018-09-10 21:24:15 +0530

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encapsulation.com.encapsulation;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            encapsu std = new encapsu();
+            std.setAgeDate(8);
+            std.setMarks(10);
+            std.setName("Rishav");
+            std.details();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/com/encapsulation/encapsu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.com.encapsulation
+{
+    class encapsu
+    {
+        private int age;
+        private int marks;
+        private string name;
+        public void setAgeDate(int a)
+        {
+            if(a > 0)
+            {
+                age = a;
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid age");
+            }
+                
+        }
+
+        public void setMarks(int a)
+        {
+            if (a > 0)
+            {
+                marks = a;
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Marks");
+            }
+
+        }
+
+        public void setName(string n)
+        {
+            if (!string.IsNullOrEmpty(n))
+            {
+                name = n;
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Name");
+            }
+
+        }
+        public void details()
+        {
+            Console.WriteLine("Nmae = {0},age ={1},marks = {2}",name,age,marks);
+        }
+    }
+}
