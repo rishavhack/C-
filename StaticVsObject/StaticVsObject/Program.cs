808120816240ce90c39536fbae7bd2a9417cbe5f
Rishav <rishav.jnit@gmail.com>
2018-10-12 21:56:23 +0530

    StaticVsObject

---
+++ StaticVsObject/StaticVsObject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVsObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new structs.Program();
+            pg.Display();
+        }
+        void Display()
+        {
+            Console.WriteLine("Hello Display");
+        }
+    }
+}
